ace85f4a7553ec93aec86c5153962b627297616e
Feldwor <BoQsc@users.noreply.github.com>
2024-01-05 19:25:44 +0200

    Update Print_Directory_Tree.cmd

--- Operating_System/Print_Directory_Tree.cmd
+++ Operating_System/Print_Directory_Tree.cmd
@@ -1,9 +1,11 @@
 @ECHO OFF
 TITLE %~nx0
 CLS
+
 ECHO %~p0
 RDMD System_Tools\Directory_Tree_Generator\Print_Directory_Tree.d
 
-ECHO Press Enter to refresh.
-PAUSE
+ECHO.
+<nul set /p "=Press any key to refresh . . ." & pause >nul
+
 CALL %~nx0
