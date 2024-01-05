cc73cea854002277607b195a493726df58b8053c
Feldwor <BoQsc@users.noreply.github.com>
2024-01-05 20:51:58 +0200

    Update Print_Directory_Tree.d

--- Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
+++ Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
@@ -2,11 +2,10 @@ import std;
 import std.regex;
 void main() {
 	
-	auto re = regex(r"[^\\]+\\","g");
     foreach (string name; dirEntries("", SpanMode.breadth))
     {
 		if (name.startsWith(".git") == false){
-			if (name.isDir) writeln(replaceAll(name, re, "  │───")); // │─ //|____
+			if (name.isDir) writeln(replaceAll(name, regex(r"[^\\]+\\","g"), "  │───")); // │─ //|____
 		}
     }
 }
