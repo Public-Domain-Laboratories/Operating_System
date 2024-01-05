bd6a54d39f0bc2cfa183a09716767f83f8a827a9
Feldwor <BoQsc@users.noreply.github.com>
2024-01-05 17:20:55 +0200

    Create Print_Directory_Tree.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

---
+++ Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
@@ -0,0 +1,13 @@
+import std;
+import std.regex;
+void main() {
+	
+	auto re = regex(r"[^\\]+\\","g");
+    foreach (string name; dirEntries("", SpanMode.breadth))
+    {
+		if (name.startsWith(".git") == false){
+			if (name.isDir) writeln(replaceAll(name, re, "|____")); // │─
+		}
+    }
+}
+
