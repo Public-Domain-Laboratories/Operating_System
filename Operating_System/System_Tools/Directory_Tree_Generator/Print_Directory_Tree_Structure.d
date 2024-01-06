6f9c07ec3776abe3c299a64d224cfa2d2112a144
Feldwor <BoQsc@users.noreply.github.com>
2024-01-06 14:40:00 +0200

    Update Print_Directory_Tree_Structure.d

--- Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree_Structure.d
+++ Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree_Structure.d
@@ -3,16 +3,19 @@ import std.regex;
 
 void main() {
 	
-    foreach (string absolutePath; dirEntries("", SpanMode.breadth))
-    {
-		if (absolutePath.startsWith(".git") == false && absolutePath.isDir){
-			if (true){
-				string expanded = replaceAll(absolutePath, regex(r"[^\\]+\\","g"), "  │───");
-				writeln(expanded); // │─ //|____ consume the paths, IF it is last folder, add newline with double spaces
-			} else if (true) {
-				writeln(absolutePath); 
-			}
-		}
-    }
+	
+	writeln(dirEntries("", SpanMode.breadth));
+	
+    //foreach (string absolutePath; dirEntries("", SpanMode.breadth))
+    //{
+	//	if (absolutePath.startsWith(".git") == false && absolutePath.isDir){
+	//		if (false){
+	//			string expanded = replaceAll(absolutePath, regex(r"[^\\]+\\","g"), "  │───");
+	//			writeln(expanded); // │─ //|____ consume the paths, IF it is last folder, add newline with double spaces. Check if it's the last folder in the path while consuming. //replaceFirst
+	//		} else if (true) {
+	//			writeln(std.regex.split(absolutePath, regex(r"[^\\]+\\","g"))); 
+	//		}
+	//	}
+    //}
 }
 
