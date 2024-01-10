12d9e8a052539ea11bb6a386c969e1c8ecb14da9
Feldwor <BoQsc@users.noreply.github.com>
2024-01-10 20:02:41 +0200

    Update Print_Directory_Tree.cmd

--- Operating_System/Print_Directory_Tree.cmd
+++ Operating_System/Print_Directory_Tree.cmd
@@ -6,12 +6,15 @@ CLS
 ECHO Directory: %~p0
 ECHO.
 
+:test_run
 FOR /F %%i IN ('DIR /S/B "TEST"') do (SET "testpath=%%~di%%~pi")
 IF ERRORLEVEL 0 (
 	FOR /F %%f IN ('DIR /S/B /OD "%testpath%*.d"') do (ECHO %%f &&  SET "testpathrun=%%f")
 	RDMD "!testpathrun!"  
 	GOTO :continue 
 )
+
+:default_run
 
 RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree.d"
 REM RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree_Structure.d"
