38f96beeeffca77b893d6ee7c32d681f4b073164
Feldwor <BoQsc@users.noreply.github.com>
2024-01-10 13:54:35 +0200

    Update

--- Operating_System/Print_Directory_Tree.cmd
+++ Operating_System/Print_Directory_Tree.cmd
@@ -6,8 +6,8 @@ CLS
 ECHO Directory: %~p0
 ECHO.
 
-REM RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree_Structure.d"
-RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Paths.d"
+RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree_Structure.d"
+REM RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Paths.d"
 
 ECHO.
 <NUL SET /p "=Press any key to refresh . . ." & PAUSE >NUL
