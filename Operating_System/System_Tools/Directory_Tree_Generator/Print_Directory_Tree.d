5064a933bf7c8d46316555226db956d9c463d140
Feldwor <BoQsc@users.noreply.github.com>
2024-01-10 14:23:48 +0200

    Update Print_Directory_Tree.d

--- Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
+++ Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
@@ -4,7 +4,10 @@ void Print_Directory_Paths(){
 	foreach (string relativePath; dirEntries("", SpanMode.breadth)){
 		if (relativePath.isDir && relativePath.startsWith(".git") == false){
 			string[] deconstructed_path = relativePath.split("\\");
-			writeln(deconstructed_path); 
+			foreach (member; deconstructed_path){
+				writeln(member); 
+			}
+			
 			// Iterate recursively and check the last member.
 		}
 	}
