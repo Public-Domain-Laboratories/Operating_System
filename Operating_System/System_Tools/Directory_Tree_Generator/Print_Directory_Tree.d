2548cbba8d6ae94f9b2967dbb55e98c325559f3b
Feldwor <BoQsc@users.noreply.github.com>
2024-01-10 14:04:32 +0200

    Update Print_Directory_Tree.d

--- Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
+++ Operating_System/System_Tools/Directory_Tree_Generator/Print_Directory_Tree.d
@@ -3,7 +3,7 @@ import std;
 void Print_Directory_Paths(){
 	foreach (string absolutePath; dirEntries("", SpanMode.breadth)){
 		if (absolutePath.isDir && absolutePath.startsWith(".git") == false){
-			writeln(absolutePath.split("\\"));
+			writeln(absolutePath.split("\\")); // Deconstruct path
 		}
 	}
 }
