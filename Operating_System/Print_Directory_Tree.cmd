26a97716cda6fb5c34b1561a0b0d213e2d6d7f74
Feldwor <BoQsc@users.noreply.github.com>
2024-01-11 18:25:14 +0200

    Update

--- Operating_System/Print_Directory_Tree.cmd
+++ Operating_System/Print_Directory_Tree.cmd
@@ -6,17 +6,22 @@ CLS
 ECHO Directory: %~p0
 ECHO.
 
-:test_run
-FOR /F %%i IN ('DIR /S/B "TEST"') DO (SET "testpath=%%~di%%~pi")
-IF ERRORLEVEL 0 (
-	FOR /F %%f IN ('DIR /S/B /OD "%testpath%*.d"') DO (SET "testpathrun=%%f")
-	RDMD "!testpathrun!"  
-	GOTO :continue 
-)
+REM :test_run
+REM FOR /F %%i IN ('DIR /S/B "TEST"') DO (SET "testpath=%%~di%%~pi")
+REM IF ERRORLEVEL 0 (
+REM 	FOR /F %%f IN ('DIR /S/B /OD "%testpath%*.d"') DO (SET "testpathrun=%%f")
+REM 	RDMD "!testpathrun!"  
+REM 	GOTO :continue 
+REM )
 
 :default_run
 
+REM 
 RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree.d"
+
+
+REM RDMD ".\System_Tools\Directory_Tree_Generator\First research\Print_Directory_Tree_deconstructedArray.d"
+REM RDMD ".\System_Tools\Directory_Tree_Generator\First research\Print_Directory_Tree_deconstructedPath.d"
 REM RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Tree_Structure.d"
 REM RDMD ".\System_Tools\Directory_Tree_Generator\Print_Directory_Paths.d"
 
