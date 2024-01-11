582baf9f318fc9e585d75b2ba941bf8969e94b71
Feldwor <BoQsc@users.noreply.github.com>
2024-01-11 18:30:50 +0200

    Update Print_Directory_Tree.d

--- Operating_System/System_Tools/Directory_Tree_Generator/while_method/Print_Directory_Tree.d
+++ Operating_System/System_Tools/Directory_Tree_Generator/while_method/Print_Directory_Tree.d
@@ -3,10 +3,10 @@ import std;
 void Print_Directory_Paths(){
 	foreach (string relativePath; dirEntries("", SpanMode.breadth)){
 		if (relativePath.isDir && relativePath.startsWith(".git") == false){
-			string[] deconstructed_path = relativePath.split(std.path.dirSeparator);
+			string[] deconstructed_path = relativePath.split(std.path.dirSeparator); // Needs to know if path is last in the folder. If it is: include round symbol.
 			foreach (member; deconstructed_path){
 				
-				if (member == deconstructed_path.back){
+				if (member == deconstructed_path.back){ 
 					write("├───"); 
 					write(member); 
 					write("\n"); 
