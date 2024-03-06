b06576eaad627402048f4035d7e98f0f93ee39a2
Feldwor <BoQsc@users.noreply.github.com>
2024-03-06 17:19:47 +0200

    Update Path_Parser.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Operating_System/System/Natural_Sort_Order/Path_Parser.d
+++ Operating_System/System/Natural_Sort_Order/Path_Parser.d
@@ -18,7 +18,7 @@ void main() {
     foreach (size_t depth, string path; paths) {
         depth=0;
         foreach (directory; splitString(path)) {
-            for (int count; count<=depth; count++) {if (count != depth) {write("  ");} else {write("|");} }
+            for (int count; count<=depth; count++) {if (count != depth) {write("|  ");} else {write("|");} }
             write("_");
             write(directory, depth++);
             write("\n");
