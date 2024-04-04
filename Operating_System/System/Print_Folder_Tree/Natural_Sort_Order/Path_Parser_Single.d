d0cec655ecad6bea49014f80b87ec79b4af021ae
Feldwor <BoQsc@users.noreply.github.com>
2024-04-04 20:45:08 +0300

    Update Path_Parser_Single.d

--- Operating_System/System/Print_Folder_Tree/Natural_Sort_Order/Path_Parser_Single.d
+++ Operating_System/System/Print_Folder_Tree/Natural_Sort_Order/Path_Parser_Single.d
@@ -17,7 +17,7 @@ void main() {
         size_t depth;
         foreach (directory; splitString(path)) {
             foreach (i; 0 .. depth) write(" ");
-            if (depth != 0) write("├");
+            if (depth != 0) write(" ");
             write(directory, ); 
             write("\n");
             depth++;
@@ -26,16 +26,22 @@ void main() {
 }
 
 // Path parser
+// Converts path to array.
+// Takes text, extracts all characters that are not a slash (/)
+// Loops over a text input and if it is not a slash character it will be stored into word and then that word is stored into an array.
 string[] splitString(string text) {
+    
+	string word;
     string[] extracted;
-    string token;
+	
     foreach (letter; text) {
         if (letter != '/'){
-            token ~= letter;
+			join([word, 's']);
+            //word ~= letter;
         }
         if (letter == '/') {
-            extracted ~= token;
-            token = "";
+            extracted ~= word;
+            word = "";
         }
     }
     return extracted;
