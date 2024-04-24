b68509c8e0e5e68a2ea53718ea0c18a6b83d7023
Feldwor <BoQsc@users.noreply.github.com>
2024-04-24 22:17:09 +0300

    Create initialize.cmd

---
+++ Operating_System/initialize.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF && TITLE Operating_System
+REM This is a Program to let Windows Operating System initialize environment to run or execute parts of Operating_System project.
+ECHO @Public Domain Laboratories, Operating_System.
