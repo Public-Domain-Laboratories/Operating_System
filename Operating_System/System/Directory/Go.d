43c6ded2557bfcb5725dfe7f92b1b3cd88bb6d58
Feldwor <BoQsc@users.noreply.github.com>
2024-04-24 20:31:18 +0300

    Update Go.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Operating_System/System/Directory/Go.d
+++ Operating_System/System/Directory/Go.d
@@ -1,4 +1,4 @@
-// Goes to a directory // "You have to go to a directory."
+// Goes to a directory // "You have to go to a directory and open this file."
 // Switches directory
 // Changes directory
 // Opens directory
