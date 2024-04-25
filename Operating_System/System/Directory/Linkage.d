4b76568b7ddc14bb1107a0963f591270826134aa
Feldwor <BoQsc@users.noreply.github.com>
2024-04-25 09:01:23 +0300

    Update Linkage.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Operating_System/System/Directory/Linkage.d
+++ Operating_System/System/Directory/Linkage.d
@@ -1,4 +1,7 @@
 // Incomplete Prototype file.
+// This whole file is around idea of accessing path variable like on Windows and Linux.
+
+
 // Combines files and folders from other directories into current directory.
 // General variable for accessible/available global files or commands from other directories or paths.
 // Accessing path variable that is used to include binaries from other directories for access and use in the current directory
