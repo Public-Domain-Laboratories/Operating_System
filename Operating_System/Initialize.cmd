3f9d29b474b9c81d2455fc1e7475e48d84813751
Feldwor <BoQsc@users.noreply.github.com>
2024-04-25 18:24:11 +0300

    update

--- Operating_System/Initialize.cmd
+++ Operating_System/Initialize.cmd
@@ -1,3 +1,5 @@
 @ECHO OFF && TITLE Operating_System
 REM This is a Program to let Windows Operating System initialize environment to run or execute parts of Operating_System project.
 ECHO @Public Domain Laboratories, Operating_System.
+ECHO This should allow you to double click the source files to execute like scripts.
+ECHO Also allow to execute source files directly.
