5c0c426d60216f86b7079219c72f4ac44d3b4647
Feldwor <BoQsc@users.noreply.github.com>
2024-04-25 08:37:38 +0300

    Update AccessPath.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Operating_System/System/Directory/AccessPath.d
+++ Operating_System/System/Directory/AccessPath.d
@@ -1,3 +1,4 @@
+// Incomplete Prototype file.
 // General variable for accessible/available global files or commands from other directories or paths.
 // Accessing path variable that is used to include binaries from other directories for access and use in the current directory
 // Foreign binaries and files access for use in the current directory.
