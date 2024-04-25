521b95b1e18b8d0dbb79da00ce077ca2bdd6e01e
Feldwor <BoQsc@users.noreply.github.com>
2024-04-25 08:24:49 +0300

    Update AccessPath.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Operating_System/System/Directory/AccessPath.d
+++ Operating_System/System/Directory/AccessPath.d
@@ -1,4 +1,4 @@
 // General variable for accessible/available global files or commands from other directories or paths.
 // Accessing path variable that is used to include binaries from other directories for access and use in the current directory
 // Foreign binaries and files access for use in the current directory.
-// IncludePath, SidePath 
+// IncludePath, SidePath, importPath
