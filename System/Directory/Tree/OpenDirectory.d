c147447ca7d50a7d34249a0500584e65f8729942
Feldwor <BoQsc@users.noreply.github.com>
2024-04-30 16:20:38 +0300

    Update OpenDirectory.d

--- System/Directory/Tree/OpenDirectory.d
+++ System/Directory/Tree/OpenDirectory.d
@@ -2,20 +2,23 @@
 import std.stdio;
 import std.file;
 
-void main()
-{
-    
-    DirEntry directory = DirEntry("C:\\Users\\Windows10\\Desktop\\Server5");
+
+void OpenDirectory(string path){
+    DirEntry directory = DirEntry(path);
     string directoryPath = directory.name();
     // dirEntries function also accepts direct raw string path.
     foreach (DirEntry entry; dirEntries(directoryPath, SpanMode.shallow))
     {
             writeln(entry.name());
     }
-
-
     write("Directory object: " ~ directory);
     writeln;
     write("Directory path: " ~ directoryPath);
+}
+
+void main()
+{
+
+    OpenDirectory("C:\\Users\\Windows10\\Desktop\\Server5");
 
 }
