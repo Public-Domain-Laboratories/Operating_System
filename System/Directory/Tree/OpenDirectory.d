90864acc8ff1dc7f5d9570f57159d7920c1ec259
Feldwor <BoQsc@users.noreply.github.com>
2024-04-30 15:19:22 +0300

    Update OpenDirectory.d

--- System/Directory/Tree/OpenDirectory.d
+++ System/Directory/Tree/OpenDirectory.d
@@ -5,7 +5,9 @@ void main()
 {
     DirEntry directory = DirEntry("C:\\Users\\Windows10\\Desktop\\Server5");
     string directoryPath = directory.name();
-    write("Directory path: " ~ directoryPath);
-    writeln;
+
     write("Directory object: " ~ directory);
+    writeln;
+    write("Directory path: " ~ directoryPath);
+
 }
