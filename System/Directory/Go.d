99f43707b8cc6fb00b918d37a5dbf96463d1273b
Feldwor <BoQsc@users.noreply.github.com>
2024-04-30 21:18:02 +0300

    Update Go.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- System/Directory/Go.d
+++ System/Directory/Go.d
@@ -2,3 +2,11 @@
 // Switches directory
 // Changes directory
 // Opens directory
+// https://dlang.org/phobos/std_file.html#chdir
+// https://github.com/dlang/phobos/blob/master/std/file.d#L2885
+
+void go(){
+  import std.file;
+  
+  
+}
