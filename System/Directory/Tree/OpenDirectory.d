84c51a4adf363808516014ef789ce1be12d16176
Feldwor <BoQsc@users.noreply.github.com>
2024-04-30 15:18:45 +0300

    Update OpenDirectory.d

--- System/Directory/Tree/OpenDirectory.d
+++ System/Directory/Tree/OpenDirectory.d
@@ -4,6 +4,8 @@ import std.file;
 void main()
 {
     DirEntry directory = DirEntry("C:\\Users\\Windows10\\Desktop\\Server5");
-    string test = directory.name();
-    write(test);
+    string directoryPath = directory.name();
+    write("Directory path: " ~ directoryPath);
+    writeln;
+    write("Directory object: " ~ directory);
 }
