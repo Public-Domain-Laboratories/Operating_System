35a8ada08882c2a74af52d9746c101418da59011
Feldwor <BoQsc@users.noreply.github.com>
2024-05-03 19:42:46 +0300

    Update Initialize.cmd

--- Initialize.cmd
+++ Initialize.cmd
@@ -1,5 +1,9 @@
-@ECHO OFF && TITLE Operating_System
+@ECHO OFF && TITLE Operating_System && COLOR 09 
 REM This is a Program to let Windows Operating System initialize environment to run or execute parts of Operating_System project.
-ECHO Operating_System@Public Domain Laboratories
+ECHO Operating_System at Public_Domain_Laboratories.
+ECHO No Rights Reserved.
+ECHO ____________________________________________
 ECHO This should allow you to execute scripts via double click on Source Files.
-ECHO Also allow to execute source files directly.
+ECHO Also allows to execute Operating_System Source Files directly.
+COLOR
+PAUSE
