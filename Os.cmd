2dffe990e9ebfac0e32d113d4fe53ee56ffee18e
Feldwor <BoQsc@users.noreply.github.com>
2024-05-05 19:51:15 +0300

    Update Os.cmd
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Os.cmd
+++ Os.cmd
@@ -1,3 +1,5 @@
 @IF NOT "%~1"=="prompt" CMD /Q /K "CLS && %~0 prompt" 
 @ECHO OFF && TITLE Operating_System && CLS
 ECHO Public_Domain_Laboratories. No Copyrights Reserved.
+
+REM This should install a executable that runs last pointed instance of Operating_System.
