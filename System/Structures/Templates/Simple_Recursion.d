526259112d59c751b83635aa411c0caa93cdd936
Feldwor <BoQsc@users.noreply.github.com>
2024-05-05 12:30:45 +0300

    Update Simple_Recursion.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- System/Structures/Templates/Simple_Recursion.d
+++ System/Structures/Templates/Simple_Recursion.d
@@ -1,6 +1,6 @@
-void recursion(int recursive_parameter=0){
+void recursion(int recursive_parameter=0){ //Recursive function
     
-    if (recursive_parameter < 10) {
+    if (recursive_parameter < 10) { // Base case
         recursion(recursive_parameter + 1); // Recursive call
     }
 }
