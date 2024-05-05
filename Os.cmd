f8bae97e302989f36e355b5d7f0d1c4e0a9e7e67
Feldwor <BoQsc@users.noreply.github.com>
2024-05-05 20:23:41 +0300

    Update Os.cmd
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Os.cmd
+++ Os.cmd
@@ -7,5 +7,7 @@ REM I think it should be Command Line interface, Command Interpreter.
 REM "os install" should  install last pointed instance of Operating_System.
 REM Maybe CMD.exe should be replaced with Command Line Interface.
 
-REM Allows to use the Command line programs immediately in your environment to script and complete work in a standard way.
+REM Allows to use the Command Line programs immediately in your environment to script and complete work in a standard way.
 REM Enables execution of .d source files. Adds ability to launch .d files from subfolders of Operating_System.
+
+REM It should pop up a blue color Command Line Interface Window.
