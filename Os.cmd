8c0e81f32eefc4e4d749c71c61fbb0765f8e9f9a
Feldwor <BoQsc@users.noreply.github.com>
2024-05-05 19:52:00 +0300

    Update Os.cmd
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Os.cmd
+++ Os.cmd
@@ -3,3 +3,4 @@
 ECHO Public_Domain_Laboratories. No Copyrights Reserved.
 
 REM This should install a executable that runs last pointed instance of Operating_System.
+REM I think it should be Command Line interface.
