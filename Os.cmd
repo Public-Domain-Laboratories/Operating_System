e11b3e576084ebf555ac6e700b0626ff2a19cc69
Feldwor <BoQsc@users.noreply.github.com>
2024-05-05 19:59:13 +0300

    Update Os.cmd
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Os.cmd
+++ Os.cmd
@@ -5,3 +5,4 @@ ECHO Public_Domain_Laboratories. No Copyrights Reserved.
 REM This should install a executable that runs last pointed instance of Operating_System.
 REM I think it should be Command Line interface, Command Interpreter.
 REM "os install" should  install last pointed instance of Operating_System.
+REM Maybe CMD.exe should be replaced with Command Line Interface.
