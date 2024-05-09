f828d89b60a61988bec7f66454eaa3f5de89c413
Feldwor <BoQsc@users.noreply.github.com>
2024-05-09 23:17:54 +0300

    Update Os.cmd
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- Os.cmd
+++ Os.cmd
@@ -17,7 +17,7 @@ REM START /I /B  "Operating_System" "CMD /Q /K"
 REM Creating new prompt does not preserve the previous environment correctly.
 
 REM Executing cmd.exe, the Operating_System should trigger as exetension and replace regular Command Prompt.
-https://github.com/microsoft/terminal/issues/1817
+REM https://github.com/microsoft/terminal/issues/1817
 
 REM This should install a executable that runs last pointed instance of Operating_System.
 REM I think it should be Command Line interface, Command Interpreter.
