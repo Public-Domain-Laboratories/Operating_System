b70a8dfc8a3f7ae6edced46b3ba5c7ba2fc81d83
Feldwor <BoQsc@users.noreply.github.com>
2024-05-09 09:16:49 +0300

    Update Os.cmd

--- Os.cmd
+++ Os.cmd
@@ -1,6 +1,7 @@
-@IF NOT "%~1"=="prompt" CMD /Q /K "CLS && %~0 prompt" 
+
 @ECHO OFF && TITLE Operating_System && CLS
 ECHO Public_Domain_Laboratories. No Copyrights Reserved.
+CMD /Q /K
 
 REM This should install a executable that runs last pointed instance of Operating_System.
 REM I think it should be Command Line interface, Command Interpreter.
