9ccb49cca78d928c4c8b3420e5170a8e515c9e6b
Feldwor <BoQsc@users.noreply.github.com>
2024-05-11 11:41:58 +0300

    Update cmdrenametest.cmd

--- cmdrenametest.cmd
+++ cmdrenametest.cmd
@@ -1,3 +1,6 @@
+@ECHO OFF
+ECHO Needs Command Prompt with elevated privilegies.
+CD "C:\Windows\system32\"
 takeown /F "C:\Windows\system32\cmd.exe" /A
 icacls "C:\Windows\system32\cmd.exe" /grant Administrators:F
 ren cmd.exe testcmd.exe
@@ -5,3 +8,4 @@ icacls "C:\Windows\system32\cmd.exe" /setowner "NT SERVICE\TrustedInstaller"
 ren testcmd.exe cmd.exe 
 icacls "C:\Windows\system32\cmd.exe" /grant:r Administrators:RX
 ren testcmd.exe cmd.exe 
+PAUSE
