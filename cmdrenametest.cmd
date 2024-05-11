ca30ca4aa3446f0fedf25d7264a8a8e2c8c10a6e
Feldwor <BoQsc@users.noreply.github.com>
2024-05-11 11:40:20 +0300

    Create cmdrenametest.cmd

---
+++ cmdrenametest.cmd
@@ -0,0 +1,7 @@
+takeown /F "C:\Windows\system32\cmd.exe" /A
+icacls "C:\Windows\system32\cmd.exe" /grant Administrators:F
+ren cmd.exe testcmd.exe
+icacls "C:\Windows\system32\cmd.exe" /setowner "NT SERVICE\TrustedInstaller"
+ren testcmd.exe cmd.exe 
+icacls "C:\Windows\system32\cmd.exe" /grant:r Administrators:RX
+ren testcmd.exe cmd.exe 
