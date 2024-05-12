1a72f575f942a345deb1fd64646b0caac13361bd
Feldwor <BoQsc@users.noreply.github.com>
2024-05-12 21:27:30 +0300

    Update Simple_Recursion.d
    Signed-off-by: Feldwor <BoQsc@users.noreply.github.com>

--- System/Structures/Templates/Simple_Recursion.d
+++ System/Structures/Templates/Simple_Recursion.d
@@ -1,9 +1,17 @@
 import std;
-void main()
-{
+
+void recursiveFunction(int times){
+    
     int counter; Recursion:
      
     writeln("Hello D");
 
     if (++counter < 5) goto Recursion;
+    
+}
+
+void main()
+{
+    recursiveFunction(5);
+
 }
