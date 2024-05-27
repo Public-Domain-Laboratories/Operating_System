830db38e4d7903447099ed3ce9e350706dc6bc6f
Feldwor <BoQsc@users.noreply.github.com>
2024-05-27 13:54:38 +0300

    Update Generate_Version.cmd

--- System/Generate_Version.cmd
+++ System/Generate_Version.cmd
@@ -1,5 +1,9 @@
 @ECHO OFF
 
+REM CALL :traverse_parent_directory
+REM ECHO DONE
+REM PAUSE
+
 @PUSHD "%~DP0..\"
 @SET /p GitHashfileContent=<".\.git\FETCH_HEAD"
 @ECHO 1136#%GitHashfileContent:~0,7%_20240527 > "%~DP0Version.txt"
@@ -11,12 +15,16 @@ REM hoursseconds#githash_yearmonthday
 EXIT /B
 
 :traverse_parent_directory
+SET "backup_CD=%CD%"
+:repeat
 FOR %%i IN ("%CD%") DO (
     IF NOT "%%~nxi"=="Operating_System" (
-        REM ECHO Visiting parent folder %CD% %CD:~1,3%
-        @CD "..\"
-        IF "%CD:~1,3%"==":\" GOTO:EOF
-        GOTO :traverse_parent_directory
+        ECHO Visiting parent folder %CD% %CD:~1,3%
+        CD "..\"
+        IF "%CD:~1,3%"==":\" CD "%backup_CD%" && GOTO:EOF
+        GOTO :repeat
+    ) ELSE ( 
+        CD "%backup_CD%" 
     )
 )
 
